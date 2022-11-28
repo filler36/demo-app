e5f0e2f4699b81b2c165e2392c2af9770d7c3046
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-11-28 16:53:04 +0100

    mod

--- feelthenotes/mainapp/views.py
+++ feelthenotes/mainapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello on the "Feel The Notes" page!')
+    return render(request, "mainapp/index.html")
