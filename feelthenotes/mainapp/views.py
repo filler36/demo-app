18b3b2ad6dc7c246c2d481ebde56087df9726388
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-11-29 12:42:16 +0100

    change docker tz

--- feelthenotes/mainapp/views.py
+++ feelthenotes/mainapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     return render(request, "mainapp/index.html", {'start_time': start_time})
 
-import time
-start_time = time.strftime('Start time: %H:%M:%S %b %d, %Y (%Z)')
+#import time
+#start_time = time.strftime('Start time: %H:%M:%S %b %d, %Y (%Z)')
+start_time = timezone.now()
