0c2d1cbf62348b96a31c129f9b8375d2bd33e473
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-11-29 14:09:41 +0100

    add bot

---
+++ feelthenotes_bot/main.py
@@ -0,0 +1,41 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start_command(update, context):
+    update.message.reply_text('Type some message here to get started...')
+
+
+def help_command(update, context):
+    update.message.reply_text('If you need help use Google!')
+
+
+def handle_message(update, context):
+    text = str(update.message.text).lower()
+    response = make_response(text)
+    update.message.reply_text(response)
+
+
+def make_response(text):
+    if text in ['hi', 'hello', 'aloha']:
+        return 'Hi dude! How is it going?'
+    if text in ['fuck', 'suck', 'dick', 'ass']:
+        return 'Ohh, fuck you man! Get outa here!'
+
+
+def error(update, context):
+    print(f"Update {update} caused error {context.error}")
+
+
+def main():
+    TELEGRAM_API_KEY = os.environ['TELEGRAM_API_KEY']
+    updater = Updater(TELEGRAM_API_KEY, use_context=True)
+    updater.dispatcher.add_handler(CommandHandler('start', start_command))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+    updater.dispatcher.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+
+
+main()
