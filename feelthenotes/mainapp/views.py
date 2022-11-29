fb8ce3a789f0176a3ad10c4b895aaca683baee1f
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-11-29 11:46:34 +0100

    mod views

--- feelthenotes/mainapp/views.py
+++ feelthenotes/mainapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "mainapp/index.html")
+    return render(request, "mainapp/index.html", {'start_time': start_time})
+
+import time
+start_time = time.strftime('Start time: %H:%M:%S %b %d, %Y')
